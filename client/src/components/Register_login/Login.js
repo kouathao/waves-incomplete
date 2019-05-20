dcc4e07790e9c948f84f35321ed4bb944c4dd3e4
Koua Thao <kouathao09@gmail.com>
2019-05-20 15:20:32 -0700

    completed 94

--- client/src/components/Register_login/Login.js
+++ client/src/components/Register_login/Login.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { update } from "../utils/Form/formActions";
+import { update, generateData, isFormValid } from "../utils/Form/formActions";
+import { loginUser } from "../../actions/user_actions";
+import { withRouter } from "react-router-dom";
 
 // components
 import FormField from "../utils/Form/formField";
@@ -51,7 +53,28 @@ class Login extends Component {
       formdata: newFormdata
     });
   };
-  submitForm = () => {};
+  submitForm = e => {
+    e.preventDefault();
+    let dataToSubmit = generateData(this.state.formdata, "Login");
+    let formIsValid = isFormValid(this.state.formdata, "Login");
+
+    if (formIsValid) {
+      this.props.dispatch(loginUser(dataToSubmit)).then(response => {
+        if (response.payload.loginSuccess) {
+          console.log(response.payload);
+          this.props.history.push("/user/dashboard");
+        } else {
+          this.setState({
+            formError: true
+          });
+        }
+      });
+    } else {
+      this.setState({
+        formError: true
+      });
+    }
+  };
 
   render() {
     return (
@@ -67,10 +90,15 @@ class Login extends Component {
             formdata={this.state.formdata.password}
             change={element => this.updateForm(element)}
           />
+
+          {this.state.formError ? (
+            <div className="error_label">Please check your data</div>
+          ) : null}
+          <button onClick={e => this.submitForm(e)}>Login</button>
         </form>
       </div>
     );
   }
 }
 
-export default connect()(Login);
+export default connect()(withRouter(Login));
